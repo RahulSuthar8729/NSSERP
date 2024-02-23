6fa587ec706024f865c819b010be92fbade4da0d
rahul.suthar <soft.dv1@narayanseva.org>
2024-02-23 17:39:24 +0530

    donor master form controller view created with design

---
+++ NSSERP/Areas/NationalGangotri/Controllers/DonorMasterController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NSSERP.Models;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using NSSERP.Areas.Masters.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics.Metrics;
+using System.Security.Claims;
+using NSSERP.Areas.NationalGangotri.Models;
+using NSSERP.DbFunctions;
+using Newtonsoft.Json;
+using static NSSERP.Areas.NationalGangotri.Models.ReceiptDetails;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.Http;
+using System.Net;
+using Mono.TextTemplating;
+using System.Text;
+using Azure.Core;
+using System.Text.Json;
+using Rotativa.AspNetCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace NSSERP.Areas.NationalGangotri.Controllers
+{
+    [Authorize]
+    [Area("NationalGangotri")]
+    public class DonorMasterController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly HttpClient _apiClient;
+        public DonorMasterController(IWebHostEnvironment webHostEnvironment, IHttpClientFactory clientFactory, HttpClient httpClient)
+
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _apiClient = clientFactory.CreateClient("WebApi");
+            _httpClientFactory = clientFactory;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Home(int id)
+        {
+            var response = await _apiClient.GetAsync($"api/DonationReceiveMaster/ViewDetails?id={id}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Response.WriteAsJsonAsync(response);
+                    // Handle other error cases if needed
+                    return StatusCode((int)response.StatusCode, $"Error: {response.ReasonPhrase}");
+                }
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var detail = json != null ? JsonConvert.DeserializeObject<DonorMaster>(json) : null;
+
+            if (detail == null)
+            {
+                return NotFound();
+            }
+
+            return View(detail);
+
+        }
+    }
+}
---
+++ NSSERP/Areas/NationalGangotri/Models/DonorMaster.cs
@@ -0,0 +1,98 @@
+﻿using NSSERP.Areas.Masters.Models;
+using System.Threading.Channels;
+
+namespace NSSERP.Areas.NationalGangotri.Models
+{
+    public class DonorMaster
+    {
+        public string DataFlag { get; set; }
+        public bool IsAnonymous { get; set; }
+        public int DonorID { get; set; }
+        public string CustId { get; set; }
+        public string UpiID { get; set; }
+        public string MailingNo { get; set; }
+
+        public string ReferenceNo { get; set; }
+        public DateTime DOE { get; set; }
+        public string DonorCat { get; set; }
+        public bool IsAppShreekaPurnVivranReceived { get; }
+
+        public string NamePrefix { get; set; } = "";
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string PrefixToRelation { get; set; }
+        public string RelationToFullName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfAnniversary { get; set; }
+        public string Company { get; set; } = "";
+        public string YourCompany { get; set; } = "";
+        public string BussinessOrJobType { get; set; } = "";
+        public string Profession { get; set; } = "";
+        public string WorkingIn { get; set; } = "";
+        public string Designation { get; set; } = "";
+        public string CareOf { get; set; } = "";
+        public decimal ToatlDonation { get; set; }
+        public string Address1 { get; set; } = "";
+        public string Address2 { get; set; } = "";
+        public string Address3 { get; set; } = "";
+        public string PinCode { get; set; } = "";
+        public int? CountryId { get; set; }
+        public string CountryName { get; set; } = "";
+        public List<Countrys> CountryList { get; set; }
+        public int? StateID { get; set; }
+        public string StateName { get; set; } = "";
+        public int? DistrictID { get; set; }
+        public string DistrictName { get; set; } = "";
+        public int? CityID { get; set; }
+        public string CityName { get; set; } = "";
+        public IEnumerable<Citys> CityList { get; set; }
+
+        public class Citys()
+        {
+            public int CityID { get; set; }
+            public string CityName { get; set; }
+        }
+        public bool? IsPermanentAddressDiff { get; set; }
+        public string P_FullAddress { get; set; } = "";
+        public string P_Address1 { get; set; } = "";
+        public string P_Address2 { get; set; } = "";
+        public string P_Address3 { get; set; } = "";
+        public string P_PinCode { get; set; } = "";
+
+        public int? P_CountryID { get; set; }
+        public string P_CountryName { get; set; } = "";
+        public int? P_StateID { get; set; }
+        public string P_StateName { get; set; } = "";
+        public int? P_DistrictID { get; set; }
+        public string P_DistrictName { get; set; } = "";
+        public int? P_CityID { get; set; }
+        public string P_CityName { get; set; } = "";
+
+        public bool IsCallActive { get; set; } = true;
+
+        public bool IsMsgActive { get; set; } = true;
+        public bool IsWhatsAppActive { get; set; } = true;
+        public bool IsEmailActive { get; set; }=true;
+        public bool IsLetterCommunicationActive { get; set; }=true;
+        public bool IsSendoperationPhotoActive { get; set; } = true;
+        public string MobileList { get; set; } = "";
+        public string IdentityList { get; set; } = "";
+
+        public string MobileListJson { get; set; } = "";
+
+        public string IdentityListJson { get; set; } = "";
+
+        public bool IsSandipanSend { get; set; }
+        public string Languages { get; set; } = "";
+        public string Sandipan { get; set; } = "";
+        public string SandipanRemarks { get; set; } = "";
+        public string ReceiptCopyRequireOptions { get; set; } = "";
+
+        public bool IsVisit { get; set; }
+        public string VisitYear { get; set; } = "";
+        public string ForginNgCodeRefrence { get; set; } = "";
+        public string ChangesRemarks { get; set; } = "";
+        public string Remarks { get; set; } = "";
+    }
+}
